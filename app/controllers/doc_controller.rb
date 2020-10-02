bd90906fc77f8681cd550ccd3efe9ac4fae511f2
Rahul-jaat <rjaat973@gmail.com>
2020-10-02 17:17:17 +0530

    path not found

---
+++ app/controllers/doc_controller.rb
@@ -0,0 +1,40 @@
+class DocController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+        @doc= Doc.new
+    end
+
+    def create
+        @doc = Doc.new(doc_params)
+
+        if @doc.save
+            redirect_to @doc
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def distroyed
+    end
+
+    private
+       
+       def find_doc
+       end
+
+       def doc_params
+        params.require(:doc).permit(:title,:content)
+       end
+    
+end
